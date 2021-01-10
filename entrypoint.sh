a0ba90f448e4cba32aee118f941fcd1a733690ea
ganariya <willwill1997willwill@yahoo.co.jp>
2021-01-10 19:59:42 +0900

    simple change

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,16 @@
 # . = /github/workspace if actions/checkout
 set -eux
 
+ls /
+pwd
+
 if [ ! -f .latexmkrc2 ]; then
     cp /.latexmkrc .latexmkrc2
+    echo "done"
 fi
 
 ls
+pwd
 
 # make pdf
 latexmk $INPUT_LATEX_FILE_NAME
